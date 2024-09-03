622277b515e99ca90d274b5ca3bced384fab0d3b
Akarshan Kumar <akarshan2602@gmail.com>
2024-09-03 19:16:20 +0530

    Added randomised tech stack for mobile.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,31 +1,24 @@
-// import React from "react";
-
-// import { BallCanvas } from "./canvas";
-// import { SectionWrapper } from "../hoc";
-// import { technologies } from "../constants";
-
-// const Tech = () => {
-//   return (
-//     <div className="flex flex-row flex-wrap justify-center gap-10">
-//       {technologies.map((technology) => (
-//         <div className="w-28 h-28" key={technology.name}>
-//           <BallCanvas icon={technology.icon} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default SectionWrapper(Tech, "");
-
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
+// Function to shuffle an array
+const shuffleArray = (array) => {
+  let shuffledArray = array.slice(); // Create a copy of the array
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
+
 const Tech = () => {
   const isMobile = window.matchMedia("(max-width: 500px)").matches;
+
+  // Shuffle technologies array
+  const shuffledTechnologies = shuffleArray(technologies);
+
   const Map = (isMobile, technologies) => {
     if (isMobile) {
       return technologies.slice(0, 4).map((technology) => (
@@ -47,13 +40,14 @@ const Tech = () => {
       ));
     }
   };
+
   return (
     <>
       <h1 className="font-bold flex justify-center text-lg mb-5 text-[#915EFF]">
         My Tech Stack
       </h1>
       <div className="flex flex-row flex-wrap justify-center gap-10">
-        {Map(isMobile, technologies)}
+        {Map(isMobile, shuffledTechnologies)}
       </div>
     </>
   );
